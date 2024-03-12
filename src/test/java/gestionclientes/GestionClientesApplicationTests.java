1590fb062dc49bbc6bddcfef347087c67e9d9bdb
brahamzapata1992 <brahamzapata1992@gmail.com>
2024-03-12 16:54:10 -0400

    subiendo

---
+++ src/test/java/gestionclientes/GestionClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package gestionclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
